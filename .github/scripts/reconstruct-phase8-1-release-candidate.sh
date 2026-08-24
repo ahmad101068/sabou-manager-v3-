bd78180490e84edbcacaa879cae74ba710236fcb
ahmad101068 <153301481+ahmad101068@users.noreply.github.com>
2026-08-24 03:45:53 +0330

    phase8.1: pin invariant-safe large-data release test

--- .github/scripts/reconstruct-phase8-1-release-candidate.sh
+++ .github/scripts/reconstruct-phase8-1-release-candidate.sh
@@ -19,7 +19,7 @@ copy_test 'data/db/Phase81RecentMigrationMatrixTest.kt' '0a88f127bc20256df5ccb05
 copy_test 'data/repository/Phase81AuditIntegrityIntegrationTest.kt' '919ce761d439ddf0a8197077ff6f5cb8a118313709fc716883cb74ada2d596a6'
 copy_test 'data/security/Phase81ForensicIntegrityLedgerIntegrationTest.kt' '4838dd0c5ba1d9ab03f515987f71c3e0280d9ca2eeaec184872005313e9a575b'
 copy_test 'data/repository/Phase81UserOptimisticConcurrencyIntegrationTest.kt' '25a8f1cf66e22543a2c67ad9134f8ab35e6fa28d4ef6c09e232c7580ede8c0b6'
-copy_test 'data/repository/Phase81LargeDataPerformanceIntegrationTest.kt' 'fe7a8620601a4537b85ed8140bef9d40f70ea0f873fbca36e45f60d00fe7a528'
+copy_test 'data/repository/Phase81LargeDataPerformanceIntegrationTest.kt' 'd99a149c624e1c678b50efe6cbd554c192a765752a7566b39f15e1ec835b6078'
 if grep -R -nE '@Ignore|@Disabled' "$root/app/src"; then
   echo '::error::ignored/disabled tests are forbidden'
   exit 1
